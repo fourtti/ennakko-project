e8b614456e538061d0a2b9e558abdbff408b9da9
fourtti <fourtti@gmail.com>
2026-06-06 17:19:08 +0300

    added try catch on async, added noResults paragraph and showing 'Ei tuloksia' when fetch has no company data, added trim() to input field values to remove spaces

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-
-//Haku buttonin 
+//Haku buttonin
 document.getElementById("searchButton").addEventListener("click", async () => {
-  const name = document.getElementById("name").value;
-  const businessId = document.getElementById("businessId").value;
+  const name = document.getElementById("name").value.trim();
+  const businessId = document.getElementById("businessId").value.trim();
 
   // api url ja kenttä lisäys
   let url = "https://avoindata.prh.fi/opendata-ytj-api/v3/companies?";
   if (name) url += "name=" + name;
   if (businessId) url += "&businessId=" + businessId;
 
-  const response = await fetch(url);
-  const data = await response.json();
+  let response;
+  let data;
+  try {
+    response = await fetch(url);
+    data = await response.json();
+  } catch (error) {
+    console.error("Haku epäonnistui:", error);
+    return;
+  }
 
- 
+  if (data.companies.length === 0) {
+    const noResults = document.getElementById("noResults");
+    noResults.innerHTML = "Ei tuloksia";
+    return;
+  }
+  // aiksemmat table entryt
+  const results = document.getElementById("results");
+
+  // tyhjenna vanhat table entryt
+  results.innerHTML = "";
 
   for (const company of data.companies) {
     const companyName = company.names[0].name;
